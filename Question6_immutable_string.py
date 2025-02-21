3c78a4b6277ac556d500cf02afe0f3d4668e9471
zaki <zzaki.ieu2023@student.ie.edu>
2025-02-21 11:26:30 +0100

    Initial

---
+++ Question6_immutable_string.py
@@ -0,0 +1,7 @@
+# Create a string
+text = "hello"
+print("Original string:", text)
+
+
+# Try modifying a character (this will cause an error)
+text[1] = "a"  # This is not allowed
---
+++ Question6_mutable_list.py
@@ -0,0 +1,18 @@
+# Create a list
+numbers = [1, 2, 3]
+print("Original list:", numbers)
+
+
+# Modify an element
+numbers[1] = 10
+print("Modified list:", numbers)
+
+
+# Add an element
+numbers.append(4)
+print("List after appending:", numbers)
+
+
+# Remove an element
+numbers.pop(0)
+print("List after removing first element:", numbers)
